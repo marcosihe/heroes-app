f39ef26d7548649b6d035f889eb03a9c60658273
marcosihe <marcosherrera.ib@gmail.com>
2021-11-24 01:56:04 -0300

    add the last path to the localstorage

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -16,7 +16,9 @@ const LoginScreen = () => {
 
     dispatch(action);
 
-    navigate("/marvel", {
+    const lastPath = localStorage.getItem('lastPath') || '/marvel'
+
+    navigate(lastPath, {
       replace: true,
     });
   };
